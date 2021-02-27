795eb4c86d6d9edfccad3c99df470d2648bd0f90
Joanna Stawiarska <goga_stonoga@o2.pl>
2021-02-27 14:44:47 +0100

    Add props for reservation form

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <ReservationForm msg="Welcome to reservation form Asiek"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ReservationForm from '@/components/ReservationForm.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    ReservationForm
   }
 }
 </script>
